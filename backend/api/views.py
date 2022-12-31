dcf89e521932c956c2ce087f730bfe1da54d819b
aniketgm <aniket.g.meshram@gmail.com>
2022-12-31 16:16:38 +0530

    [DjangoReactApp] backend and frontend initial code

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 # Create your views here.
 
@@ -40,3 +42,16 @@ def get_routes(request):
     ]
     return Response(routes)
 
+
+@api_view(["GET"])
+def get_notes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_note(request, pk):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
